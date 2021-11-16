d23cbfc2441cc388111f7b698208cc1bbf7d8cae
Botond Kulcsar <bkulcsar@gmx.com>
2021-11-16 12:06:45 +0200

    exercise 9.1

---
+++ bmiCalculator.ts
@@ -0,0 +1,32 @@
+
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / (height / 100) ** 2;
+   
+    if (bmi < 16) {
+        return 'Underweight (Severe thinness)'
+    }
+    if (bmi < 17) {
+        return 'Underweight (Moderate thinness)'
+    }
+    if (bmi < 18.5) {
+        return 'Underweight (Mild thinness)'
+    }
+    if (bmi < 25) {
+        return 'Normal range'
+    }
+    if (bmi < 30) {
+        return 'Overweight (Pre-obese)'
+    }
+    if (bmi < 35) {
+        return 'Obese (Class I)'
+    }
+    if (bmi < 40) {
+        return 'Obese (Class II)'
+    }
+    if (bmi >= 40) {
+        return 'Obese (Class III)'
+    }
+    return 'Out of range'
+}
+
+console.log(calculateBmi(180, 70));
